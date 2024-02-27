7fec1d18204e92526cf7c6c5f0122d3d0b4559f0
ffaline <155386093+ffaline@users.noreply.github.com>
2024-02-27 19:47:18 +0100

    New changes

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+let playerScore = 0
+let computerScore = 0
+
 function getComputerChoice () {
      let number = Math.floor(Math.random()*3)
      let comChoice = "";
@@ -19,29 +23,35 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "rock")
      console.log("It's a tie!")
 
-    else if (playerSelection === "rock" && computerSelection === "paper")
+    else if (playerSelection === "rock" && computerSelection === "paper"){
     console.log("You Lose! Paper beats Rock!")
+    computerScore += 1}
 
-    else if (playerSelection === "scissors" && computerSelection === "paper")
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
     console.log("You Win! Scissors beat paper")
+    playerScore += 1}
     
-    else if (playerSelection === "rock" && computerSelection === "scissors")
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
     console.log("You Win! Rock beats Scissors!")
+    playerScore += 1}
 
     else if (playerSelection === "scissors" && computerSelection === "scissors")
     console.log("It's a Tie!")
 
-    else if (playerSelection === "scissors" && computerSelection === "rock")
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
     console.log("You Lose! Rock beats Scissors!")
+    computerScore += 1}
 
     else if (playerSelection === "paper"   && computerSelection === "paper")
     console.log("It's a Tie!")
 
-    else if (playerSelection === "paper"   && computerSelection === "rock")
+    else if (playerSelection === "paper"   && computerSelection === "rock"){
     console.log("You win! Paper beats Rock!")
+    playerScore += 1}
 
-    else if (playerSelection === "paper"   && computerSelection === "scissors")
+    else if (playerSelection === "paper"   && computerSelection === "scissors"){
     console.log ("You Lose! Scissors beat Paper!")
+    computerScore += 1}
 
 }
 
@@ -52,10 +62,10 @@ let playerSelection = temporary.toLowerCase();
 playRound(playerSelection, getComputerChoice);
 }
 
-playGame(getComputerChoice());
-playGame(getComputerChoice());
-playGame(getComputerChoice());
-playGame(getComputerChoice());
-playGame(getComputerChoice());
+for (let i = 0; i < 5; i++){
+playGame(getComputerChoice());}
 
+if (playerScore >= 3)
+console.log("Congratulations! You win!")
 
+else console.log("Computer Wins!")
